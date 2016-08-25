d1e4872e5bb37187ba88296da0cd0dd087dc0a73
Lee-PC\Lee <hanzen927@gmail.com>
2016-08-26 02:25:09 +0900

    KT은행

---
+++ KTbank/src/net/gondor/Account.java
@@ -0,0 +1,22 @@
+package net.gondor;
+
+public class Account {
+	private int money;
+	private String accountNum;;
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public String getAccountNum() {
+		return accountNum;
+	}
+
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+}
---
+++ KTbank/src/net/gondor/Customer.java
@@ -0,0 +1,13 @@
+package net.gondor;
+
+public class Customer {
+	private int money;
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+}
