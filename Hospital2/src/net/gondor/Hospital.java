f3a7c01490f397b7b21c21f804542a334a92b20a
Lee-PC\Lee <hanzen927@gmail.com>
2016-08-26 02:13:07 +0900

    KT은행

--- Hospital2/src/net/gondor/Hospital.java
+++ Hospital2/src/net/gondor/Hospital.java
@@ -7,59 +7,71 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Hospital {
-	private List<String> doctors = new ArrayList<String>();
-	private Map<String, List<String>> patient = new HashMap<String, List<String>>();
-	private Scanner input = new Scanner(System.in);
-	
-	public List<String> getDoctors() {
-		return doctors;
-	}
-	public void setDoctors(List<String> doctors) {
-		this.doctors = doctors;
-	}
-	public Map<String, List<String>> getPatient() {
-		return patient;
-	}
-	public void setPatient(Map<String, List<String>> patient) {
-		this.patient = patient;
-	}
-	public void showDoctor() {
-		System.out.println("================================");
-		for(int i =0; i<doctors.size();i++){
-		int pSize = patient.get(doctors.get(i)).size();
-		System.out.println((i+1)+". "+doctors.get(i)+"("+pSize+")");
-		}
-		System.out.println("================================");
-	}
 	/**
-	 * doctors 리스트에 의사 이름을 추가합니다.
-	 * @param doctorName
+	 * 의사들의 이름을 저장하는 배열리스트
 	 */
-	public void addDoctor(String doctorName){
+	private List<String> doctors = new ArrayList<String>();
+	/**
+	 * 의사들의 이름을 키로 저장하고
+	 * 값으로 배열리스트 타입을 저장함으로써 1:다수의 저장이 가능합니다.
+	 * doctors를 key로 두고 value로 환자를 한 명씩 add할 수 있습니다.
+	 */
+	private Map<String, List<String>> patient = new HashMap<String, List<String>>();
+
+	/**
+	 * doctors 배열리스트에 의사 이름을 추가하는 메소드
+	 * 또한 Map에도 키로서 추가를 합니다.
+	 * @param doctorName add와 put을 동시에 합니다.
+	 */
+	public void addDoctor(String doctorName) {
 		doctors.add(doctorName);
 		patient.put(doctorName, new ArrayList<String>());
-		System.out.println("\""+doctorName+"\""+"를 등록합니다.");
+		System.out.println("\"" + doctorName + "\"" + "를 등록합니다.");
 		showDoctor();
 	}
-	public boolean addPatient(String doctorName, String patientName){
-		boolean isDoctor =doctors.contains(doctorName);
-		if(isDoctor){
+
+	/**
+	 * 의사에게 환자를 추가시켜주는 메소드	 *
+	 * @param doctorName : 의사
+	 * @param patientName : 환자  배열리스트 추가(add)
+	 * @return
+	 */
+	public boolean addPatient(String doctorName, String patientName) {
+		boolean isDoctor = doctors.contains(doctorName);
+		if (isDoctor) {
 			patient.get(doctorName).add(patientName);
-			System.out.println("입력하신 \""+patientName+"\"환자는 "
-		+"\""+doctorName+"\"에게 등록되었습니다.");
+			System.out.println("입력하신 \"" + patientName + "\"환자는 " + "\"" + doctorName + "\"에게 등록되었습니다.");
 		}
-		else{
+		else {
 			System.out.println("잘못 입력하셨습니다.");
 		}
 		return isDoctor;
 	}
-	public void showPatient(String doctorName){
-	System.out.println("\"" + doctorName + "\"의 환자목록입니다.");
-	int pSize=patient.get(doctorName).size();
-	System.out.println("================================");
-	for(int i=0; i<pSize;i++){
-		System.out.println((i+1)+". "+patient.get(doctorName).get(i));
+	
+	/**
+	 * 의사목록을 보여주는 메소드
+	 * 의사가 담당하는 환자의 수도 알 수 있습니다.
+	 */
+	public void showDoctor() {
+		System.out.println("================================");
+		for (int i = 0; i < doctors.size(); i++) {
+			int pSize = patient.get(doctors.get(i)).size();
+			System.out.println((i + 1) + ". " + doctors.get(i) + "(" + pSize + ")");
+		}
+		System.out.println("================================");
 	}
-	System.out.println("================================");
+	
+	/**
+	 * 환자목록을 보여주는 메소드
+	 * @param doctorName
+	 */
+	public void showPatient(String doctorName) {
+		System.out.println("\"" + doctorName + "\"의 환자목록입니다.");
+		int pSize = patient.get(doctorName).size();
+		System.out.println("================================");
+		for (int i = 0; i < pSize; i++) {
+			System.out.println((i + 1) + ". " + patient.get(doctorName).get(i));
+		}
+		System.out.println("================================");
 	}
 }
--- Hospital2/src/net/gondor/Main.java
+++ Hospital2/src/net/gondor/Main.java
@@ -7,39 +7,44 @@ public class Main {
 	private final int addPatient = 2;
 	private final int showPatients = 3;
 	private final int exit = 4;
-	private int selNum;
-
+	
+	/**
+	 * 의사나 환자의 추가, 조회를 총괄적으로 담당하는 메소드
+	 */
 	public void management() {
-
 		Scanner input = new Scanner(System.in);
 		Hospital hos = new Hospital();
-
-		String name = "";
-		String name2 = "";
+		String patientName = "";
+		String doctorName = "";
+		int selNum = 0;
 		while (true) {
 			menu();
 			selNum = input.nextInt();
 			input.nextLine();
+			//nextInt는 숫자만 공백이나 줄띄기는 무시합니다.
+			//그러므로 input.nextLine의 역할은 줄띄기입니다. 입력 기능은 무시됩니다.
 			if (selNum == addDoctor) {
 				System.out.println("의사등록을 진행합니다.\n의사의 이름을 입력하세요.");
 				while (true) {
-					name = input.nextLine();
-					if (name.equals("")) {
+					doctorName = input.nextLine();
+					if (doctorName.equals("")) {
 						System.out.println("아무것도 입력하지 않았습니다.\n의사의 이름을 입력하세요.");
-					} else {
-						hos.addDoctor(name);
+					} 
+					else {
+						hos.addDoctor(doctorName);
 						break;
 					}
 				}
 			} 
 			else if (selNum == addPatient) {
 				System.out.println("환자를 등록합니다.\n환자의 이름을 입력하세요.");
-				name = input.next();
+				patientName = input.next();
 				hos.showDoctor();
 				while (true) {
 					System.out.println("원하시는 의사의 이름을 입력하세요.");
-					name2 = input.next();
-					if (hos.addPatient(name2, name)) {
+					doctorName = input.next();
+					if (hos.addPatient(doctorName, patientName)) {
+						//의사의 이름을 잘 입력해야 환자의 추가가 올바르게 됩니다
 						break;
 					}
 				}
@@ -47,8 +52,9 @@ public class Main {
 			else if (selNum == showPatients) {
 				System.out.println("환자를 조회합니다.\n의사의 이름을 입력하세요.");
 				hos.showDoctor();
-				name = input.next();
-				hos.showPatient(name);
+				//입력을 돕기위해 의사목록을 보여줍니다.
+				doctorName = input.next();
+				hos.showPatient(doctorName);
 			} 
 			else if (selNum == exit) {
 				System.out.println("안녕히 가세요.");
@@ -57,6 +63,9 @@ public class Main {
 		}
 	}
 
+	/**
+	 * 선택 메뉴를 출력합니다.
+	 */
 	private void menu() {
 		System.out.println("원하는 업무를 선택하세요.");
 		System.out.println("================================");
