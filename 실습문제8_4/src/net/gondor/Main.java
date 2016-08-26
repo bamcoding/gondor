3e2e84c26a611df08f0af2b6e8ec03f1fc1f3bab
206-002\206-002 <hanzen927@gmail.com>
2016-08-26 17:33:55 +0900

    no message

--- 실습문제8_4/src/net/gondor/Main.java
+++ 실습문제8_4/src/net/gondor/Main.java
@@ -6,14 +6,24 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		List<ClassList> classLists = new ArrayList<ClassList>();
+		List<TeamMembers> tempAddress = new ArrayList<TeamMembers>();		
 		classLists.add(new ClassList("1반"));
-		classLists.get(0).getTeamMembers().add(new TeamMembers("이근재", "010119", "미정", "미정", "본인","한국"));
+		tempAddress = classLists.get(0).getTeamMembers();
+		tempAddress.add(new TeamMembers("이근재", "010119", "미정", "미정", "본인","한국"));
+		tempAddress.add(new TeamMembers("이근재2", "010119", "미정", "미정", "본인","한국"));
+		
 		classLists.add(new ClassList("2반"));
-		classLists.get(1).getTeamMembers().add(new TeamMembers("리카르도", "010119", "미정", "미정", "본인","영국"));
+		tempAddress = classLists.get(1).getTeamMembers();
+		tempAddress.add(new TeamMembers("리카르도", "010119", "미정", "미정", "본인","영국"));
+		
 		classLists.add(new ClassList("3반"));
-		classLists.get(2).getTeamMembers().add(new TeamMembers("호도", "010119", "미정", "미정", "본인","미국"));
+		tempAddress = classLists.get(2).getTeamMembers();
+		tempAddress.add(new TeamMembers("호도", "010119", "미정", "미정", "본인","미국"));
+		tempAddress.add(new TeamMembers("호도2", "010119", "미정", "미정", "본인","미국"));
+		
 		classLists.add(new ClassList("4반"));
-		classLists.get(3).getTeamMembers().add(new TeamMembers("데끼마스", "010119", "미정", "미정", "본인","일본"));
+		tempAddress = classLists.get(3).getTeamMembers();
+		tempAddress.add(new TeamMembers("데끼마스", "010119", "미정", "미정", "본인","일본"));
 
 		for (int i = 0; i < classLists.size(); i++) {
 			System.out.println(classLists.get(i));
